db8f8456630d0b491c06c14944ed7988d5a51806
1paleking1 <1paleking1@gmail.com>
2023-06-27 21:26:53 +0100

    CircularList class now works with the three implemented methods working as indented.

--- CircularList.py
+++ CircularList.py
@@ -2,16 +2,21 @@ class CircularList:
 
     def __init__(self, lst):
         self.__lst = lst
+        self.__reverse_lst = lst[::-1]
+        self.__current_index = 0
 
-    def __str__(self):
-        return self.__lst
-    
-    def __getitem__(self, i):
-        return self.__lst[i % len(self.__lst)]        
+    def set_current_index(self, index):
+        if (index * -1) <= len(self.__lst):
+            self.__current_index = index
+        else:
+            raise IndexError("Index out of range. Index must be less than or equal to the length of the list.")
 
+    def get_next_right(self):
+        item = self.__lst[self.__current_index]
+        self.__current_index = (self.__current_index + 1) % len(self.__lst)
+        return item
 
-c = CircularList([1,2,3])
-
-count = 0
-for i in c:
-    print(i)
+    def get_next_left(self):
+        item = self.__reverse_lst[self.__current_index]
+        self.__current_index = (self.__current_index + 1) % len(self.__reverse_lst)
+        return item
