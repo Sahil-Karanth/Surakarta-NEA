23837fa93eb6905956c182ecb1187d91e5d5e1e4
1paleking1 <1paleking1@gmail.com>
2023-08-17 13:29:38 +0100

    added some board constants and changed method orders and added docstrings to Graphical_UI class. Did not go through the methods that make pages aand have layouts will do this another time

--- Basic GUI (stage 2)/UI.py
+++ Basic GUI (stage 2)/UI.py
@@ -171,20 +171,18 @@ class Graphical_UI(UI):
 
         sg.theme('DarkTanBlue')
 
-        self.__current_page = "home_page"
         self.__highlighted_board_positions = []
 
         self.__window = self.__setup_home_page()
 
+        self.__game = None
 
-    def __create_window(self, title, layout, justification, transparent=False):
-        
-        if transparent:
-            transparent_colour = sg.theme_background_color()
-        else:
-            transparent_colour = None
 
-        window = sg.Window(
+    def __create_window(self, title, layout, justification):
+
+        """Creates a window with the given title, layout and justification"""
+
+        self.__window = sg.Window(
             title=title,
             layout=layout,
             size=(700, 700),
@@ -192,32 +190,31 @@ class Graphical_UI(UI):
             keep_on_top=True,
             margins=(20,20),
             element_justification=justification,
-            # transparent_color=transparent_colour,
-            # alpha_channel=0.1,
-            text_justification=justification # ! might break things check this
+            text_justification=justification # ! when writing help page check if I need this
         ).finalize()
 
-        # window.TKroot.attributes("-transparentcolor", "black")
+        self.__maximise_window()
 
-        # sg.LOOK_AND_FEEL_TABLE['Transparent'] = {'BACKGROUND': '#FFFFFF00'}
-        # sg.ChangeLookAndFeel('Transparent')
-
-
-        # full screen without the maximise animtation
-        window.TKroot.geometry("{0}x{1}+0+0".format(window.TKroot.winfo_screenwidth(), window.TKroot.winfo_screenheight()))
-
-        self.__window = window
-        return window
+        return self.__window
 
     def get_UI_type(self):
         return self.__UI_type
     
+    def __maximise_window(self):
+
+        """Maximises the window without an animation"""
+
+        self.__window.TKroot.geometry("{0}x{1}+0+0".format(self.__window.TKroot.winfo_screenwidth(), self.__window.TKroot.winfo_screenheight()))
+    
     def __setup_home_page(self):
 
+        """Creates the home page window"""
+
+        button_pad = (15, 10)
         buttons_layout = [
-            [sg.Button("New Game", pad=(15, 10), font=(self.FONT, self.BUTTON_SIZE), size=self.BUTTON_DIMENSIONS, key="new_game_button")],
-            [sg.Button("Help", pad=(15, 10), font=(self.FONT, self.BUTTON_SIZE), size=self.BUTTON_DIMENSIONS, key="help_button",)],
-            [sg.Button("Exit", pad=(15, 10), font=(self.FONT, self.BUTTON_SIZE), size=self.BUTTON_DIMENSIONS, key="exit_button")],
+            [sg.Button("New Game", pad=button_pad, font=(self.FONT, self.BUTTON_SIZE), size=self.BUTTON_DIMENSIONS, key="new_game_button")],
+            [sg.Button("Help", pad=button_pad, font=(self.FONT, self.BUTTON_SIZE), size=self.BUTTON_DIMENSIONS, key="help_button",)],
+            [sg.Button("Exit", pad=button_pad, font=(self.FONT, self.BUTTON_SIZE), size=self.BUTTON_DIMENSIONS, key="exit_button")],
         ]
 
         buttons_frame = sg.Frame(title="", layout=buttons_layout, border_width=3, pad=(0, self.COLUMN_PAD))
@@ -229,9 +226,12 @@ class Graphical_UI(UI):
         ]
 
 
+        # needs to be returned as this is the first window created
         return self.__create_window("Surakarta", layout, "center")
     
     def __create_menu(self):
+
+        """Creates the menu which is displayed on every page"""
 
         menu_layout = [
             ["Utilities", ["Home"]],
@@ -242,6 +242,7 @@ class Graphical_UI(UI):
 
     def __setup_new_game_page(self):
         
+        """Creates the new game page window"""
         
         AI_input_layout = [
             [sg.Text("Difficulty", pad=(0, self.COLUMN_PAD), font=self.SUBHEADING_FONT_PARAMS)],
@@ -273,6 +274,8 @@ class Graphical_UI(UI):
 
     def __setup_help_page(self):
 
+        """Creates the help page window"""
+
         text_layout = [
             [sg.Text("What is Surakarta?", pad=(0, self.COLUMN_PAD), font=self.SUBHEADING_FONT_PARAMS)],
             [sg.Text(self.__dummy_text, pad=(0, self.COLUMN_PAD), font=self.PARAGRAPH_FONT_PARAMS)],
@@ -290,10 +293,14 @@ class Graphical_UI(UI):
         ]
 
         self.__window.close()
-
         self.__window = self.__create_window("Help Page", layout, "center")
 
     def __toggle_play_inputs(self, key_to_make_visible):
+
+        """Toggles between the AI and local play inputs"""
+
+        # ! haven't grouped the submit buttons into their respective frames because of formatting issues that come with this
+
         if key_to_make_visible == "AI_play_inputs":
             self.__window["local_play_inputs"].update(visible=False)
             self.__window["AI_play_inputs"].update(visible=True)
@@ -309,8 +316,11 @@ class Graphical_UI(UI):
     def __make_piece_button(self, piece_type, key, visible=False):
         return sg.Button("", image_filename=f"{piece_type}_counter.png", visible=visible, pad=(30,30), key=key, button_color=(sg.theme_background_color(), sg.theme_background_color()), border_width=0)
 
-    def __setup_match_page(self, display_board):
+    def __setup_match_page(self):
 
+        """Creates the match page window where the game is played"""
+
+        display_board = [[i.get_colour() for i in row] for row in self.__game.get_board_state()]
         board_layout = []
 
         for i, row in enumerate(display_board):
@@ -322,10 +332,10 @@ class Graphical_UI(UI):
                     button = self.__make_piece_button("blank", key, visible=True)
 
                 elif counter == "y":
-                    button = self.__make_piece_button("y", key, visible=True)
+                    button = self.__make_piece_button(BoardConstants.PLAYER_1_COLOUR, key, visible=True)
 
                 elif counter == "g":
-                    button = self.__make_piece_button("g", key, visible=True)
+                    button = self.__make_piece_button(BoardConstants.PLAYER_2_COLOUR, key, visible=True)
 
                 board_layout.append(button)
 
@@ -351,7 +361,122 @@ class Graphical_UI(UI):
 
         self.__create_window("Match", layout, "center")
 
-    def run_gui(self):
+
+
+    def __make_move_on_display(self, values):
+
+        """updates the onscreen board and game object board with the move made"""
+
+        start_cords = self.__str_key_to_cords_tuple(self.__highlighted_board_positions[0])
+        end_cords = self.__str_key_to_cords_tuple(self.__highlighted_board_positions[1])
+
+        start_loc = self.__game.get_board_state()[start_cords[0]][start_cords[1]]
+        end_loc = self.__game.get_board_state()[end_cords[0]][end_cords[1]]
+
+        if values["move_type_radio_move"]:
+            move_type = "move"
+        
+        elif values["move_type_radio_capture"]:
+            move_type = "capture"
+
+        else:
+            sg.popup("Please select a move type", keep_on_top=True)
+            self.__toggle_highlight_board_position(self.__highlighted_board_positions[1])
+            self.__toggle_highlight_board_position(self.__highlighted_board_positions[0])
+            return
+
+        if self.__game.is_legal_move(start_loc, end_loc, move_type):
+
+            self.__update_board_display(start_loc, end_loc)
+            self.__game.move_piece(start_loc, end_loc, move_type)
+
+            self.__update_current_player_display()
+            self.__game.switch_current_player()
+
+            self.__game.set_game_status()
+            
+            # if move_type == "capture": # ! optimisation to use instead of above
+            #     self.__game.set_game_status()
+
+            if self.__game.is_game_over():
+                winning_player = self.__game.get_winner()
+
+                if winning_player == None:
+                    sg.popup("The game was a draw!", keep_on_top=True)
+                else:
+                    sg.popup(f"{winning_player.get_name()} has won the game!", keep_on_top=True)
+
+
+        else:
+            sg.popup("ILLEGAL MOVE", keep_on_top=True)
+
+        self.__toggle_highlight_board_position(self.__highlighted_board_positions[1])
+        self.__toggle_highlight_board_position(self.__highlighted_board_positions[0])
+
+        self.__highlighted_board_positions = []
+
+
+
+    def __update_board_display(self, start_loc, end_loc):
+
+        """updates the onscreen board with the move made"""
+    
+        start_cords = f"{start_loc.get_cords()[0]},{start_loc.get_cords()[1]}"
+        end_cords = f"{end_loc.get_cords()[0]},{end_loc.get_cords()[1]}"
+
+        self.__window[f"{start_cords}"].update(image_filename=f"blank_counter.png")   
+        self.__window[f"{end_cords}"].update(image_filename=f"{start_loc.get_colour()}_counter.png") 
+
+
+    def __update_current_player_display(self):
+
+        """updates the onscreen current player display"""
+
+        current_text = self.__window["player1_turn_text"]
+
+        if self.__game.get_current_player_name() == self.__game.get_player1_name():
+            current_text.update(f"{self.__game.get_player2_name()}'s Turn")
+        
+        elif self.__game.get_current_player_name() == self.__game.get_player2_name():
+            current_text.update(f"{self.__game.get_player1_name()}'s Turn")
+
+
+    def __is_board_position(self, key):
+
+        """checks if the key of a GUI element i a board position meaning it is a tuple containing two elements where each element is a digit from 0 to 5 inclusive""" 
+
+        pattern = r'^[0-5],[0-5]$'
+        if bool(re.match(pattern, key)):
+            return True
+        
+    def __str_key_to_cords_tuple(self, string_key):
+
+        """converts a string key of a GUI element in the form 'x,y' to a tuple of the form (x,y) where x and y are integers"""
+
+        return tuple(int(i) for i in string_key.split(","))
+    
+
+    def __toggle_highlight_board_position(self, key):
+
+        """toggles the background colour of a board position between white and pink.
+        No more than two board positions can be highlighted at once."""
+            
+        button = self.__window[key]
+
+        if key in self.__highlighted_board_positions:
+            button.update(button_color=('pink', sg.theme_background_color()))
+            self.__highlighted_board_positions.remove(key)
+
+        elif key not in self.__highlighted_board_positions and len(self.__highlighted_board_positions) < 2:
+            button.update(button_color=(sg.theme_background_color(), 'pink'))
+            self.__highlighted_board_positions.append(key)
+
+
+    def __create_game_object(self, name1, name2):
+        self.__game = Game(name1, name2)
+
+
+    def play_game(self):
         while True:
             event, values = self.__window.read()
             if event == sg.WIN_CLOSED or event == 'Cancel':
@@ -370,8 +495,8 @@ class Graphical_UI(UI):
                 self.__toggle_play_inputs("local_play_inputs")
 
             elif event == "submit_local_play_button":
-                self.__setup_game(values["player_1_name_input"], values["player_2_name_input"])
-                self.play_game()
+                self.__create_game_object(values["player_1_name_input"], values["player_2_name_input"])
+                self.__setup_match_page()
 
             elif event == "submit_AI_play_button":
                 # print("AI SUBMIT")
@@ -379,7 +504,6 @@ class Graphical_UI(UI):
 
             elif self.__is_board_position(event):
                 self.__toggle_highlight_board_position(event)
-
 
             elif event == "Home":
                 self.__setup_home_page() # ! FIXME doesn't fully work (doesn't close the window)
@@ -390,137 +514,10 @@ class Graphical_UI(UI):
         self.__window.close()
 
 
-    def __make_move_on_display(self, values):
-        start_cords = self.__str_key_to_cords_tuple(self.__highlighted_board_positions[0])
-        end_cords = self.__str_key_to_cords_tuple(self.__highlighted_board_positions[1])
-
-        print(start_cords)
-        print(end_cords)
-
-        # sys.exit()
-
-        start_loc = self.__game.get_board_state()[start_cords[0]][start_cords[1]]
-        end_loc = self.__game.get_board_state()[end_cords[0]][end_cords[1]]
-
-        if values["move_type_radio_move"]:
-            move_type = "move"
-        
-        elif values["move_type_radio_capture"]:
-            move_type = "capture"
-
-        else:
-            sg.popup("Please select a move type", keep_on_top=True)
-            self.__toggle_highlight_board_position(self.__highlighted_board_positions[1])
-            self.__toggle_highlight_board_position(self.__highlighted_board_positions[0])
-            return
-
-        if self.__game.is_legal_move(start_loc, end_loc, move_type):
-            print("MOVE IS LEGAL")
-
-            self.__update_board(start_loc, end_loc)
-            self.__game.move_piece(start_loc, end_loc, move_type)
-
-            self.__update_current_player_display()
-            self.__game.switch_current_player()
-
-            self.__game.set_game_status()
-            
-            # if move_type == "capture":
-            #     self.__game.set_game_status()
-
-            if self.__game.is_game_over():
-                winning_player = self.__game.get_winner()
-
-                if winning_player == None:
-                    sg.popup("The game was a draw!", keep_on_top=True)
-                else:
-                    sg.popup(f"{winning_player.get_name()} has won the game!", keep_on_top=True)
 
 
-
-        else:
-            sg.popup("ILLEGAL MOVE", keep_on_top=True)
-
-        # (for loop doesn't work because the list you iterate over gets changed)
-        self.__toggle_highlight_board_position(self.__highlighted_board_positions[1])
-        self.__toggle_highlight_board_position(self.__highlighted_board_positions[0])
-
-
-        self.__highlighted_board_positions = []
-
-
-
-    def __update_board(self, start_loc, end_loc):
-    
-        start_cords = f"{start_loc.get_cords()[0]},{start_loc.get_cords()[1]}"
-        end_cords = f"{end_loc.get_cords()[0]},{end_loc.get_cords()[1]}"
-
-        self.__window[f"{start_cords}"].update(image_filename=f"blank_counter.png")   
-        self.__window[f"{end_cords}"].update(image_filename=f"{start_loc.get_colour()}_counter.png") 
-
-
-    def __update_current_player_display(self):
-
-        # self.__window["player1_turn_text"].update(visible=not self.__window["player1_turn_text"].visible)
-        # self.__window["player2_turn_text"].update(visible=not self.__window["player2_turn_text"].visible)
-
-        current_text = self.__window["player1_turn_text"]
-
-        if self.__game.get_current_player_name() == self.__game.get_player1_name():
-            current_text.update(f"{self.__game.get_player2_name()}'s Turn")
-        
-        elif self.__game.get_current_player_name() == self.__game.get_player2_name():
-            current_text.update(f"{self.__game.get_player1_name()}'s Turn")
-
-
-
-
-    def __is_board_position(self, key):
-        # if it's a tuple containing two elements where each element is a digit from 0 to 5 inclusive
-        # use regex
-        pattern = r'^[0-5],[0-5]$'
-        if bool(re.match(pattern, key)):
-            return True
-        
-    def __str_key_to_cords_tuple(self, string_key):
-        return tuple(int(i) for i in string_key.split(","))
-    
-
-
-
-
-    def __toggle_highlight_board_position(self, key):
-            
-        button = self.__window[key]
-
-        if key in self.__highlighted_board_positions:
-            print("unhighlighting")
-            button.update(button_color=('white', sg.theme_background_color()))
-            self.__highlighted_board_positions.remove(key)
-
-        elif len(self.__highlighted_board_positions) == 2:
-            print("max highlighted positions reached")
-            return
-
-        elif key not in self.__highlighted_board_positions:
-            print("making pink")
-            button.update(button_color=('white', 'pink'))
-            self.__highlighted_board_positions.append(key)
-
-
-    def __setup_game(self, name1, name2):
-        self.__game = Game(name1, name2)
-
-    def play_game(self):
-
-        display_board = [[i.get_colour() for i in row] for row in self.__game.get_board_state()]
-
-        for i in display_board:
-            print(i)
-
-        self.__setup_match_page(display_board)
 
 
 ui = Graphical_UI()
 
-ui.run_gui()
+ui.play_game()
